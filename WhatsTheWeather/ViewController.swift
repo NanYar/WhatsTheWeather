b2562619e46cfb50392699a51a905f751d2f9f49
NanYar <nanyar@macbookpro113.fritz.box>
2014-11-18 21:38:49 +0100

    Completed code according to tutorial

--- WhatsTheWeather/ViewController.swift
+++ WhatsTheWeather/ViewController.swift
@@ -7,19 +7,67 @@
 //
 
 import UIKit
+//import Foundation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var weatherMessageLabel: UILabel!
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func weatherButtonPressed(sender: UIButton)
+    {
+        self.view.endEditing(true) // Retract the keyboard
+        
+        var cityString = cityTextField.text.capitalizedString
+        cityString = cityString.stringByReplacingOccurrencesOfString(" ", withString: "", options: nil, range: nil)
+        
+        let urlString = "http://www.weather-forecast.com/locations/" + cityString + "/forecasts/latest"
+        let url = NSURL(string: urlString)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!)
+        {
+            (data, response, error) in
+            
+            if let urlContent = NSString(data: data, encoding: NSUTF8StringEncoding)
+            {
+                if urlContent.containsString("<span class=\"phrase\">")
+                {
+                    let contentArray = urlContent.componentsSeparatedByString("<span class=\"phrase\">")
+                    let contentOfSecondHalfArray = contentArray[1].componentsSeparatedByString("</span>")
+                    
+                    let weatherForecast = contentOfSecondHalfArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                    
+                    dispatch_async(dispatch_get_main_queue())
+                    {
+                        self.weatherMessageLabel.hidden = false
+                        self.weatherMessageLabel.text = cityString + "\n1 – 3 Day Weather Forecast:\n\n" + weatherForecast
+                        self.cityTextField.text = ""
+                    }
+                }
+                else
+                {
+                    dispatch_async(dispatch_get_main_queue())
+                    {
+                        self.weatherMessageLabel.hidden = false
+                        self.weatherMessageLabel.text = "😱\nCould not find \"" + self.cityTextField.text + "\"\nPlease try again!"
+                        self.cityTextField.text = ""
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
 
 }
-
